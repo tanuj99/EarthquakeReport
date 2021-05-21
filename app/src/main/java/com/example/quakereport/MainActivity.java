c4d35f99728e7432213a0dc6c41ba1f7496ba963
Tanuj Soni <tanujsoni08@gmail.com>
2021-05-21 16:15:05 +0530

    Adding HTTp Request

--- app/src/main/java/com/example/quakereport/MainActivity.java
+++ app/src/main/java/com/example/quakereport/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,36 +12,66 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private EarthquakeAdapter mAdapter;
+
+    private static final  String USGS_REQUEST_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Earthquake> earthquakes = EarthquakeQuery.extractEarthquakes();
-
-
-
+        //ArrayList<Earthquake> earthquakes = EarthquakeQuery.extractFeaturefromJson();
         // Find a reference to the {@link ListView} in the layout
+
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
 
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this,earthquakes);
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
+
+        //EarthquakeAdapter adapter = new EarthquakeAdapter(this,earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(mAdapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Earthquake currentEarthquake = adapter.getItem(position);
+                Earthquake currentEarthquake = mAdapter.getItem(position);
                 Uri earthquakeUri = Uri.parse(currentEarthquake.getmUrl());
                 Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
 
                 startActivity(websiteIntent);
             }
         });
+
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute(USGS_REQUEST_URL);
+    }
+    private class EarthquakeAsyncTask extends AsyncTask<String , Void , List<Earthquake>> {
+
+        @Override
+        protected List<Earthquake> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<Earthquake> result = EarthquakeQuery.fetchEarthquakeData(urls[0]);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            //super.onPostExecute(earthquakes);
+
+            mAdapter.clear();
+
+            if(earthquakes != null && !earthquakes.isEmpty()){
+                mAdapter.addAll(earthquakes);
+            }
+        }
     }
 }
